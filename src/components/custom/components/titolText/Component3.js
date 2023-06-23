1229e322662d5906142f33a8b850a74a7861f324
Marti Serarols Tarres <m.serarolst@gmail.com>
2023-06-23 13:13:04 +0200

    3 columnes

---
+++ src/components/custom/components/titolText/Component3.js
@@ -0,0 +1,53 @@
+import { Box, Container, Divider, Grid, Typography } from "@mui/material";
+import Image from "next/image";
+import { valor } from "..";
+
+export default function Component3({ component, matches, imatges, theme, router, ...props }) {
+	return (
+		<Box style={{ backgroundColor: component.dark ? theme.palette.primary.main : theme.palette.background.main }} {...props}>
+			<Container maxWidth={'xl'} style={{padding: '10%'}}>
+                <Grid container spacing={8}>
+                    <Grid item md={4} xs={12}>
+                        <Box>
+                        <Typography variant="h2" textAlign={"left"} fontWeight={"bold"} textTransform={"unset"} mb={4} color={component.dark ? "white" : "black"}>
+                            {valor(0, component)}
+                        </Typography>
+                            <Typography
+                                dangerouslySetInnerHTML={{ __html: valor(1, component) }}
+                                variant="body1"
+                                textAlign={"left"}
+                                color={component.dark ? "white" : "black"}
+                            ></Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item md={4} xs={12}>
+                        <Box>
+                        <Typography variant="h2" textAlign={"left"} fontWeight={"bold"} textTransform={"unset"} mb={4} color={component.dark ? "white" : "black"}>
+                            {valor(2, component)}
+                        </Typography>
+                            <Typography
+                                dangerouslySetInnerHTML={{ __html: valor(3, component) }}
+                                variant="body1"
+                                textAlign={"left"}
+                                color={component.dark ? "white" : "black"}
+                            ></Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item md={4} xs={12}>
+                        <Box>
+                        <Typography variant="h2" textAlign={"left"} fontWeight={"bold"} textTransform={"unset"} mb={4} color={component.dark ? "white" : "black"}>
+                            {valor(4, component)}
+                        </Typography>
+                            <Typography
+                                dangerouslySetInnerHTML={{ __html: valor(5, component) }}
+                                variant="body1"
+                                textAlign={"left"}
+                                color={component.dark ? "white" : "black"}
+                            ></Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+			</Container>
+		</Box>
+	);
+}
