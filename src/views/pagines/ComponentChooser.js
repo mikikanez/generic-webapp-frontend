2ab3eaa2a504d7816ed3385ed94790d11cd83f12
miquel_escanez <miquel.escanez@gmail.com>
2023-07-07 17:21:22 +0200

    Pre rendering 10

--- src/views/pagines/ComponentChooser.js
+++ src/views/pagines/ComponentChooser.js
@@ -2,6 +2,6 @@ import { components } from "@/components/custom/components";
 import ComponentWrap from "@/components/custom/components/ComponentWrap";
 
 export default function ComponentChooser({ com, ...props }) {
-	const Component = components.filter((c) => c.id === Number(com.component_id))?.[0]?.component;
+	const Component = components.filter((c) => c.id === com.component_id)?.[0]?.component;
 	return <ComponentWrap Component={Component} component={com} {...props} />;
 }
